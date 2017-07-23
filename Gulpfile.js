5aed11ea9891b21c55fb9b3d3d83e56d9574f2bd
Yotau Hong <crazymousethief@outlook.com>
2017-07-24 01:46:40 +0800

    Add eslint, update gulp and some settings

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,14 +1,14 @@
-var gulp = require('gulp'),
-    browserSync = require('browser-sync').create();
-gulp.task('browser-reload', function (done) {
-    browserSync.reload();
-    done();
+var gulp = require("gulp"),
+  browserSync = require("browser-sync").create();
+gulp.task("browser-reload", function(done) {
+  browserSync.reload();
+  done();
 });
-gulp.task('default', function () {
-    browserSync.init({
-        server: {
-            baseDir: "./"
-        }
-    });
-    gulp.watch(["**/*"], ['browser-reload']);
-})
+gulp.task("default", function() {
+  browserSync.init({
+    server: {
+      baseDir: "./"
+    }
+  });
+  gulp.watch(["**/*"], ["browser-reload"]);
+});
