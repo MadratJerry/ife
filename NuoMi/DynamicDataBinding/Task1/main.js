ccd7455ee9bf0aa8f045b6da56e39868ddf867f8
Yotau Hong <crazymousethief@outlook.com>
2017-09-04 23:14:48 +0800

    Update NuoMi/DynamicDataBinding/Task1

--- NuoMi/DynamicDataBinding/Task1/main.js
+++ NuoMi/DynamicDataBinding/Task1/main.js
@@ -9,70 +9,42 @@ class Observer {
    * @memberof Observer
    */
   constructor(data) {
-    this.data = data
-    this.walk(data)
-  }
-  /**
-   * Recursive assignment
-   * @param {Object} value
-   * @memberof Observer
-   */
-  walk(value) {
-    Object.keys(value).forEach(key => this.convert(key, value[key]))
-  }
-  /**
-   * Convert the Object to Observer
-   * @param {String} key
-   * @param {Object} value
-   * @memberof Observer
-   */
-  convert(key, value) {
-    Observer.defineReactive(this.data, key, value)
-  }
-  /**
-   *
-   * @static
-   * @param {Object} value
-   * @returns {Observer}
-   * @memberof Observer
-   */
-  static observe(value) {
-    if (typeof value === 'object') return new Observer(value)
-  }
-  /**
-   * Bind the object getter and setter
-   * @static
-   * @param {Object} obj
-   * @param {String} key
-   * @param {Object} value
-   * @memberof Observer
-   */
-  static defineReactive(obj, key, value) {
-    Observer.observe(value)
-    Object.defineProperty(obj, key, {
-      enumerable: true,
-      configurable: true,
-      get() {
-        console.log('You visit ' + key)
-        return value
+    Object.keys(data).forEach(key => {
+      if (typeof data[key] === 'object') data[key] = new Observer(data[key])
+    })
+    return new Proxy(data, {
+      /**
+       * A trap for getting property values.
+       * @param {Object} target
+       * @param {String} property
+       * @returns
+       */
+      get(target, property) {
+        console.log(`Visit ${property}`)
+        return target[property]
       },
-      set(newValue) {
-        console.log(`You set ${key}`)
-        console.log(`New ${key} = ${newValue}`)
-        if (newValue === value) return
-        value = newValue
-        Observer.observe(newValue)
+      /**
+       * A trap for setting property values.
+       * @param {Object} target
+       * @param {String} property
+       * @param {any} value
+       */
+      set(target, property, value) {
+        console.log(`Change ${property} = ${value}`)
+        target[property] =
+          typeof value == 'object' ? new Observer(value) : value
       }
     })
   }
 }
 
-let data = {
-  user: {
-    name: 'crazymousethief',
-    age: 100
+let ob = new Observer({
+  name: {
+    lastname: 'a',
+    firstname: 'b'
   },
-  country: 'China'
-}
+  age: 100
+})
 
-let ob = new Observer(data)
+ob.name
+ob.age = 101
